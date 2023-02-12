129d359f6040b707c76f30b233709bb6ea0318ef
ElZacho200a <75814263+ElZacho200a@users.noreply.github.com>
2023-02-12 19:43:07 +0100

    Add files via upload
    allé lààà

---
+++ Sortie.cs
@@ -0,0 +1,76 @@
+﻿namespace Moteur
+{
+    internal class Sortie : Entity
+    {
+
+        public int nextLevel = Level.currentLevel.ID;
+         protected Point trigger;
+        bool Used = true;
+        public bool LevelLoad = false;
+       
+        public Sortie(int nextLevel , int x , int y) { 
+        
+            this.nextLevel = nextLevel;
+            this.Coordonates = (x, y);
+            var blocH = Level.blocH;
+            Hitbox = new Rectangle(x, y, 50, 50);
+           trigger = new Point( x  + (blocH / 2) ,y + blocH/2 );
+        }
+
+        public override void Update()
+        {
+           
+            if (Camera.player.Hitbox.IntersectsWith(Hitbox))
+            {
+                
+                if (!Used)
+                    LoadNextLevel();
+                Used = true;
+
+            }
+            else
+            {
+                Used= false;
+            }
+        }
+
+
+        protected void LoadNextLevel()
+        {
+            LevelLoad= true;
+            Level.currentLevel.Deactivate();
+            var ID = Level.currentLevel.ID;
+            
+            var nLevel = new Level(nextLevel );
+            Type t = this.GetType();
+            foreach (var entity in nLevel.GetEntities())
+            {
+                if(entity.GetType() == t )
+                {
+                       
+                    var sortie = (Sortie)entity;
+                        
+                    if (sortie.nextLevel == ID)
+                    {
+                        
+                       
+
+
+                        Level.currentLevel.destroy();
+                       
+                        Level.currentLevel = nLevel;
+                        
+                        Camera.player.Coordonates = (sortie.Coordonates.x, sortie.Coordonates.y + Level.blocH - Camera.player.Hitbox.Height);
+                        Camera.player.Hitbox.Location = new Point(Camera.player[0], Camera.player[1]);
+                        Camera.ResetScope();
+                        Level.currentLevel.Activate();
+                        
+                        return;
+                    }
+                       
+                }
+
+            }
+        }
+    }
+}
