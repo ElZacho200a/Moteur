a14843c8c67a2fedc804f7a619d35ff3f0112b9f
marc <marc.dessevre@epita.fr>
2023-04-14 16:07:50 +0200

    J'ai ajouté un fichier pour nous permettre de rajouter des sons. Je vais m'occuper d'en trouver qui correspondent bien à chaques mouvements.

---
+++ Moteur/SoundManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Forms.VisualStyles;
+
+namespace Moteur;
+using NAudio.Wave;
+
+public class SoundManager
+{
+    private string path = Directory.GetCurrentDirectory().Split("bin")[0] + @"Assets\Sounds\";
+
+    private void garbage(Mp3FileReader reader, WaveOut waveout)
+    {
+        waveout.Dispose();
+        reader.Dispose();
+    }
+
+    private void playUntilEnd(WaveOut waveOut, Mp3FileReader reader)
+    {
+        waveOut.PlaybackStopped += (sender, eventArgs) =>
+        {
+            waveOut.Stop();
+            garbage(reader, waveOut);
+        };
+        waveOut.Play();
+    }
+    public void jumpSong()
+    {
+        Mp3FileReader reader =
+            new Mp3FileReader(path + "Jump.mp3");
+        
+        WaveOut waveout = new WaveOut();
+        
+        waveout.Init(reader);
+        
+        playUntilEnd(waveout,reader);
+    }
+}
